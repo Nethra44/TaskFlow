923fc53af7bd9a2c20eca0f719ab29f1d07eeed3
Nethra C <nethrapandi23@gmail.com>
2026-07-05 18:17:55 +0530

    Fix CORS and forgot password logging

--- server/index.js
+++ server/index.js
@@ -6,7 +6,12 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // 1. Database Connection

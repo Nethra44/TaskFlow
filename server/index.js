aae6acda04189d536066b17ae8995edeb6819680
Nethra C <nethrapandi23@gmail.com>
2026-07-05 12:48:08 +0530

    Use Render port

--- server/index.js
+++ server/index.js
@@ -150,4 +150,8 @@ app.delete("/api/tasks/:id", async (req, res) => {
 app.get("/test", (req, res) => {
   res.send("Server is running!");
 });
-app.listen(5000, () => console.log("🚀 Server running on Port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(` Server running on Port ${PORT}`);
+});

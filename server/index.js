de65727866b5d870070fbdf7b5927fab79f04aa5
Nethra C <nethrapandi23@gmail.com>
2026-07-06 13:04:56 +0530

    Update email configuration

--- server/index.js
+++ server/index.js
@@ -51,13 +51,14 @@ const Task = mongoose.model("Task", TaskSchema);
 
 // 3. Email Config
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 transporter.verify((error, success) => {
   if (error) {

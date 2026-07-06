75e6f726cd889b57b978fc37e2f23f2b98b80e61
Nethra C <nethrapandi23@gmail.com>
2026-07-06 21:50:34 +0530

    Update CORS for Vercel

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://taskflow-client-ealh.onrender.com",
+      "https://task-flow-weld-tau.vercel.app",
     ],
     credentials: true,
   }),
